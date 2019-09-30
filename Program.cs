b0522d26a8d83de8c9165844727aa28b8f236e14
taylor18915 <54372304+taylor18915@users.noreply.github.com>
2019-09-30 14:50:10 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace classy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+         var prog=new String (
+        
+        }
+    }
+    class Animals
+    {
+
+        public string _name { get; set; }
+        public int _breed { get; set; }
+        public Animals(string name, int breed)
+
+        {
+            _name = name;
+            _breed = breed
+
+
+
+    }
+        public void walk()
+        {
+            Console.WriteLine("I am walking");
+
+        }
+        class cats
+        {
+            public string _name { get; set; }
+            public string _sixtoe { get; set; }
+        }
+        public void animals()
+        {
+            Console.WriteLine("");
+
+          }
+        public void beagel()
+        {
+        Console.WriteLine("I like beagels")
+        }
+    }
